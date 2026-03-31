0e0a90b7141263e7dd3d2fc61adfc747618adbef
anton <jappinen.anton@protonmail.com>
2026-03-31 20:04:15 +0200

    fixed vulnerbilities

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -89,10 +90,15 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
+		Addr:    port,
 		Handler: router,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout: 10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout: 60 * time.Second,
 	}
 
-	log.Printf("Serving on port: %s\n", port)
+	safePort := strings.ReplaceAll(port, "\n", "")
+	log.Printf("Serving on port: %s", &safePort)
 	log.Fatal(srv.ListenAndServe())
 }
